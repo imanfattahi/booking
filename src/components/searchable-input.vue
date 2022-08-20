1ea4d8ab9321d48d89e587dfd206f0f9ec5c287a
iman fattahi <imanfava@gmail.com>
2022-08-20 14:07:04 +0400

    Bootstrap 5 added

---
+++ src/components/searchable-input.vue
@@ -0,0 +1,20 @@
+<template>
+   <input class="form-control" type="text" v-model="input" placeholder="Search city..." />
+  <div class="item" v-for="item in filteredList()" :key="item">
+    <p>{{ item }}</p>
+  </div>
+  <div class="item error" v-if="input&&!filteredList().length">
+     <p>No results found!</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+let input = ref("");
+const items = ["apple", "banana", "orange"];
+function filteredList() {
+  return items.filter((item) =>
+    item.toLowerCase().includes(input.value.toLowerCase())
+  );
+}
+</script>
