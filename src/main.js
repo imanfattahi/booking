94b431e78bcc4cb55f44723b3238d4f0f27773a4
iman fattahi <imanfava@gmail.com>
2022-08-20 21:32:49 +0400

    Axios is added to project

--- src/main.js
+++ src/main.js
@@ -2,12 +2,23 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios';
 
 import "bootstrap"
 import './assets/main.scss'
 import 'vue-next-select/dist/index.min.css'
 
+// Create Axios client
+const $http = axios.create({
+  baseURL: 'http://127.0.0.1:3000',
+  headers: {}
+});
+
 const app = createApp(App)
+
+// global properties
+app.config.globalProperties.$http = $http
+store.$http = $http
 
 app
   .use(router)
